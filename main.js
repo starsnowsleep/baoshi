b070e575f6f6151398948401957419ca3e5eb5b0
starsnowsleep <ejhding@outlook.com>
2024-12-10 19:20:10 +0800

    add markdown css

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-// // main.js
+/**
+ * 
+ * Copyright (C) 2024 starsnowsleep
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 document.addEventListener('DOMContentLoaded', () => {
     const editorBody = document.getElementById("editor-body");
     const textarea = document.getElementById('edit');
@@ -6,9 +22,10 @@ document.addEventListener('DOMContentLoaded', () => {
         if (event.key === 'Escape') {
             document.getElementById('preview').innerHTML =
                 marked.parse(textarea.value);
-            editorBody.classList.remove('edit-mode');
-        } else if (event.key === 'i' || event.key === 'I') {
-            editorBody.classList.add('edit-mode');
+            editorBody.classList.toggle('edit-mode');
+            if (editorBody.classList.contains('edit-mode')) {
+                textarea.focus();
+            }
         }
     });
 });
